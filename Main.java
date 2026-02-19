b55df68b3755e559e36bfbacb20e03a803a1e986
anubhav <arshukla326@gmail.com>
2026-02-19 23:19:32 +0530

    changes

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
  public static void main(String[] args){
   System.out.println("This is the test via jenkins");
 System.out.println("This is the test via push");
+System.out.println("Success");
  }
 }
